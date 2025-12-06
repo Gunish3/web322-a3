7e2361dd981741f9ac7f240e624e6bb41f4975b6
Gunish <gunish@Gunishs-MacBook-Air.local>
2025-12-05 21:10:30 -0500

    Fix static path and Vercel routing

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // static folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(
   clientSessions({
